93126c20bc82892308d99c7b8555a91403ee855e
breakfastmeansbreakfast <77744c@gmail.com>
2018-09-26 21:22:10 +0100

    completed find function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
+const artist = require('./controllers/Artist');
 
 require('dotenv').config({
   path: path.join(__dirname, './settings.env'),
@@ -11,5 +12,8 @@ const app = express();
 mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true });
 app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello MongoDb!'));
+app.post('/artist', artist.post);
+app.get('/artist', artist.list);
+app.get('/artist/:artistId', artist.get);
 
 app.listen(3000, () => console.log('It works!'));
